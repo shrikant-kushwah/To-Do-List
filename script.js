4271fce7cc1fba5e724d0b1225f08b77eb659d63
Shrikant Kushwah <96473205+shrikant-kushwah@users.noreply.github.com>
2023-05-19 13:28:56 +0530

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+document.querySelector(".add").onclick = function()
+{
+    if(document.querySelector(".inputbox input").value == 0)
+    {
+        alert("Please Write Something ! ")
+    }
+    else
+    {
+        let list = document.querySelector(".list");
+        let task = document.querySelector("#task").value;
+        list.innerHTML +=`
+        <div class="new_list">
+        <div class="check">
+            <input type="checkbox" id="checkbox">
+        </div>
+        <div class="text">
+            <p>${Task}</p>
+        </div>
+        <div class="del">
+            <i class="fa-regular fa-x"></i>
+        </div>
+    </div>
+        `;
+        var del = document.querySelectorAll(".del");
+        for(var i=0; i<del.length; i++)
+        {
+            del[i].onclick = function()
+            {
+                this.parentNode.remove();
+            }
+        }
+    }
+}
